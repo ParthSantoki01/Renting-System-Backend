36dbda11f164f0bf746b34335e180cf5d7b234da
Utkarsh Asari <utkarshasari99@gmail.com>
2021-04-03 01:08:15 +0530

    get request on order schema

--- routes/order.js
+++ routes/order.js
@@ -3,6 +3,8 @@ const order = require('../models/order')
 
 const express = require('express')
 const router = express.Router()
+const Product = require("../models/product");
+
 
 router.post('/checkout',async(req,res)=> {
     
@@ -16,5 +18,53 @@ router.post('/checkout',async(req,res)=> {
         console.log('Sucessfully bought product')
         res.redirect('/')
 });
+router.get("/:orderId", (req, res, next) => {
+    Order.findById(req.params.orderId)
+      .exec()
+      .then(order => {
+        if (!order) {
+          return res.status(404).json({
+            message: "Order not found"
+          });
+        }
+        res.status(200).json({
+          order: order,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/orders"
+          }
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+  router.get("/:firstname", (req, res, next) => {
+    Order.findById(req.params.firstname)
+      .exec()
+      .then(order => {
+        if (!order) {
+          return res.status(404).json({
+            message: "Order not found"
+          });
+        }
+        res.status(200).json({
+          order: order,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/orders"
+          }
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 module.exports = router;
+
+
